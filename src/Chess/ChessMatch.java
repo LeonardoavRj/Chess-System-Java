21454ba81ab5a95bcd67fb3e9b48259b25a84681
Leonardo Vieira <Leonardovieira54@hotmail.com>
2020-08-16 17:53:05 -0300

    Possible moves of a pieces 1

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -43,6 +43,9 @@ public class ChessMatch {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("There is not pieces on source position");
 		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is not possible moves for the chosen pieces");
+		}
 		
 	}
 	
