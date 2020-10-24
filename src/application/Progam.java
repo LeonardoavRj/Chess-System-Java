d84c0bdd0d66a88d034f03303245ddf9584e08c4
Leonardo Vieira <Leonardovieira54@hotmail.com>
2020-10-24 12:47:52 -0300

    Special move - Promotion

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -40,6 +40,12 @@ public class Progam {
 				if(capturedPieces != null) {
 					captured.add(capturedPieces);
 				}
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			
 			 }
 			 catch(ChessException e){
